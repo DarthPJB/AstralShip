d9bedb840385b9e6b4d384110f960c1ddf26bedf
John Bargman <darthpjb@gmail.com>
2020-08-13 15:10:01 +0100

    Fixed infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -4,7 +4,8 @@ killall -9 xwinwrap
 for (( ; ; ))
 do
     echo "LAUNCHING PID"
-    xwinwrap -b -fs -sp -fs -nf -ov -- mplayer ~/Pictures/wallpaper/infinity.mp4 -wid WID &  echo $! > infinity.pid
+    xwinwrap -b -ov -nf -s -ni -g 1920x1080+0+0  -- mplayer ~/Pictures/wallpaper/infinity.mp4 -loop 0 -wid WID & echo $! > infinity.pid
+    xwinwrap -b -ov -nf -s -ni -g 1920x1080+1920+0  -- mplayer ~/Pictures/wallpaper/infinity.mp4 -loop 0 -nosound -wid WID &  echo $! > infinity2.pid
     CurPID=$(<"infinity.pid")
     echo "pid is $CurPID"
     tail -f --pid=$CurPID -f /dev/null
